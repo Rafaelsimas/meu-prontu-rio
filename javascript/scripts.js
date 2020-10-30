397e5752082787390fa6e40bd2fefecd357b0fd1
rafael simas <rafael.simas@al.resilia.com.br>
2020-10-30 00:29:17 -0300

    todos os arquivos modificados

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,3 +10,18 @@ function logar(){
     } 
 }
 logar()
+
+
+function solicitar() {
+    const nome = document.getElementById('nome').value
+    const sobrenome = document.getElementById('sobrenome').value
+    const endereco = document.getElementById('endereco').value
+    const identidade = document.getElementById('identidade').value
+    const sintomas = document.getElementById('sintomas').value
+    
+    
+    document.getElementById('root').innerHTML = `PACIÊNTE: ${nome} ${sobrenome} <br> ENDEREÇO:${endereco} <br> RG:${identidade} <br> SINTOMAS:${sintomas}`
+    
+    }
+   
+   
